9ad461d9df44e9ff429881dbd6902fa405219d66
AmbiNtsoah <ambinints04@gmail.com>
2025-05-22 17:47:36 +0300

    feat: migration all entity into the DB

---
+++ src/Entity/Preference.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PreferenceRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PreferenceRepository::class)]
+class Preference
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $langue = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $theme = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?bool $notification = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getLangue(): ?string
+    {
+        return $this->langue;
+    }
+
+    public function setLangue(?string $langue): static
+    {
+        $this->langue = $langue;
+
+        return $this;
+    }
+
+    public function getTheme(): ?string
+    {
+        return $this->theme;
+    }
+
+    public function setTheme(?string $theme): static
+    {
+        $this->theme = $theme;
+
+        return $this;
+    }
+
+    public function isNotification(): ?bool
+    {
+        return $this->notification;
+    }
+
+    public function setNotification(?bool $notification): static
+    {
+        $this->notification = $notification;
+
+        return $this;
+    }
+}
